3465616627a5c23c668208ffd7516bcbf0336432
CamilaLoli <u202110385@upc.edu.pe>
2024-09-21 14:42:31 -0500

    chore: added dependencies.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import {PrimeVue} from "@primevue/core";
+import i18n from "@/i18n.js";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {ripple: true});
+
+app.use(i18n);
+
+app.mount('#app');
