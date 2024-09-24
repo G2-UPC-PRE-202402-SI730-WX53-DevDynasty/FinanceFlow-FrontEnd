661f9a6df813eea637fdf8c734b2fe97a1639419
CamilaLoli <u202110385@upc.edu.pe>
2024-09-24 10:08:17 -0500

    fix: fixed endpoint for navigation.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,48 @@
-/*import {createRouter} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
+import roleSelectionComponent from "@/team-activities/components/role-selection.component.vue";
+import leaderComponent from "@/team-activities/components/leader.component.vue";
+import memberComponent from "@/team-activities/components/member.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/team', name:'team-activities'
+        // Home
+
+
+        // Restaurants
+
+
+
+
+
+
+
+        // Orders
+
+
+
+
+
+        //Team Activities
+
+
+
+
+
+
+        // Profile
+
+
+        // Notifications
+
+
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'GastroGo';
+    document.title = `${baseTitle} - ${to.meta['title']}`;
+    next();
 })
 
-*/
+export default router;
