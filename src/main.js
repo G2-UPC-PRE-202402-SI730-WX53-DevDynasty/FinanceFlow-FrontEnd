bf5a67571a4a3dddd4de616d93c1dfc6de15664e
CamilaLoli <u202110385@upc.edu.pe>
2024-09-24 09:15:18 -0500

    chore: added use of routes.

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {PrimeVue} from "@primevue/core";
 import i18n from "@/i18n.js";
+import router from "@/routes/index.js";
 
 const app = createApp(App);
 
 app.use(PrimeVue, {ripple: true});
 
 app.use(i18n);
+
+app.use(router);
 
 app.mount('#app');
