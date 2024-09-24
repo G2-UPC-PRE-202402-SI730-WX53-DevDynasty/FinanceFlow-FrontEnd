880b91fb85fc78874ae585ffea39623c222dee82
CamilaLoli <u202110385@upc.edu.pe>
2024-09-24 09:13:46 -0500

    chore: added index.js content for routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,21 @@
-/*import {createRouter} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
+import roleSelectionComponent from "@/team-activities/components/role-selection.component.vue";
+import leaderComponent from "@/team-activities/components/leader.component.vue";
+import memberComponent from "@/team-activities/components/member.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/team', name:'team-activities'
+        { path: '/team-activities', name:'team-activities', component: roleSelectionComponent, meta: {title: 'Team Activities'}},
+        { path: '/team-activities/leader', name:'team-activities-leader', component: leaderComponent, meta: {title: 'Leader'}},
+        { path: '/team-activities/member', name:'team-activities-member', component: memberComponent, meta: {title: 'Member'}},
+        ]
+});
+
+router.beforeEach((to, from, next) => {
+  let baseTitle = 'GastroGo';
+  document.title = `${baseTitle} - ${to.meta['title']}`;
+  next();
 })
 
-*/
+export default router;
