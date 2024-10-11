059e1fd2d743582ae14209e15f60c5d0cc5de065
Ricardo <richiabv@gmail.com>
2024-10-11 18:45:47 -0500

    feat(plan-restaurant): created plan restaurants component

---
+++ src/iam/components/plan-restaurant.component.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "PlanRestaurantComponent",
+}
+</script>
+
+<template>
+  <div class="login-page">
+    <div class="background-section">
+      <img src="../../assets/backgroung-introduction.png" alt="Restaurant Image" class="restaurant-image" />
+      <div class="background-text">
+        <h2>
+          Transform your meals <br>
+          into a flavoury <br>
+          experience&nbsp;beyond&nbsp;your <br>
+          imaginations and get it <br>
+          quickly!
+        </h2>
+      </div>
+    </div>
+    <div class="login-section">
+      <h1>This is your plan</h1>
+      <div class="section-plan">
+        <p>A plan specially made for your restaurant to increase its sellings. Just one more step and the benefits will be all yours!</p>
+        <ul>
+          <li>Advertise your restaurant with us!</li>
+          <li>Have an in-real time tracker for each order to avoid issues</li>
+          <li>Prioritize large groups of orders</li>
+        </ul>
+        <p>All for S/. 400 monthly</p>
+      </div>
+      <button type="submit" class="go-to-login-button">Go back to Log In</button>
+    </div>
+    <img src="../../assets/logo-gastrogo.png" alt="GastroGo Logo" class="Logo" />
+  </div>
+</template>
+
+<style>
+
+
+</style>
