659ed2ceeacd21e98309c7d494ad4088d9f6c3b1
Ricardo <richiabv@gmail.com>
2024-10-11 17:40:38 -0500

    feat(successful-recover-password): created successful recover password component

---
+++ src/iam/components/successful-recover-password.component.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "SuccessfulRecoverPasswordComponent",
+}
+</script>
+
+<template>
+  <div class="login-page">
+    <div class="background-section">
+      <img src="../../assets/backgroung-introduction.png" alt="Restaurant Image" class="restaurant-image" />
+      <div class="background-text">
+        <h2>
+          Transform your meals <br>
+          into a flavoury <br>
+          experience&nbsp;beyond&nbsp;your <br>
+          imaginations and get it <br>
+          quickly!
+        </h2>
+      </div>
+    </div>
+    <div class="login-section">
+      <h1>Recover your Password</h1>
+      <form>
+        <div class="form-group">
+          <label for="email">An email was sent to the email address provided</label>
+        </div>
+        <button type="submit" class="go-to-login-button">Go back to Log In</button>
+      </form>
+    </div>
+    <img src="../../assets/logo-gastrogo.png" alt="GastroGo Logo" class="Logo" />
+  </div>
+</template>
+<style scoped>
+
+</style>
