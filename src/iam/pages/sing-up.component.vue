574c0502c9c6b8b369988c5de8424910d7652b73
Ricardo <richiabv@gmail.com>
2024-10-11 18:14:06 -0500

    feat(sing-up): created sing-up component

---
+++ src/iam/pages/sing-up.component.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "SingUpComponent",
+}
+
+</script>
+
+<template>
+  <div class="login-page">
+    <div class="background-section">
+      <img src="../../assets/backgroung-introduction.png" alt="Restaurant Image" class="restaurant-image" />
+      <div class="background-text">
+        <h2>
+          Transform your meals <br>
+          into a flavoury <br>
+          experience&nbsp;beyond&nbsp;your <br>
+          imaginations and get it <br>
+          quickly!
+        </h2>
+      </div>
+    </div>
+
+    <div class="section-singup">
+      <h1>What are you interested in?</h1>
+      <div class="option-group">
+        <button class="option-button">I am an employee!</button>
+        <p>Order a whole menu for each of your coworkers at the palm of your hand.</p>
+        <button class="option-button">I am the owner of a restaurant!</button>
+        <p>Advertise your restaurant, increase the number of clients monthly and <br> have a good organization of each order.</p>
+      </div>
+    </div>
+    <img src="../../assets/logo-gastrogo.png" alt="GastroGo Logo" class="Logo" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
