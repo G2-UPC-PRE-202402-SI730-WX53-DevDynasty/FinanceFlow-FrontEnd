09a0ad95c5b2c4ffde02d294ca42d405057ef6b8
Ricardo <richiabv@gmail.com>
2024-10-11 18:44:48 -0500

    feat(plan-workers): added styles for plan workers

--- src/iam/components/plan-workers.component.vue
+++ src/iam/components/plan-workers.component.vue
@@ -47,5 +47,129 @@ export default {
 
 
 <style scoped>
+.login-page {
+  display: flex;
+  height: 100vh;
+}
 
+.background-section {
+  width: 720px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+.plans-description {
+  font-size: 20px;
+}
+.restaurant-image {
+  width: 110%;
+  height: 100vh;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: -302px;
+}
+
+.background-text {
+  position: relative;
+  z-index: 1;
+  padding: 20px;
+  max-width: 450px;
+  text-align: left;
+  left: -290px;
+
+}
+
+.background-text h2 {
+  font-size: 36px;
+  line-height: 1.2;
+  margin: 0;
+
+}
+
+.login-section {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: #fdeae8;
+  padding: 20px;
+
+}
+
+.Logo {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 150px;
+}
+
+.login-section h1 {
+  font-size: 45px;
+  margin: 20px 0;
+  text-align: center;
+}
+
+.plan-section {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 20px 0;
+  gap: 30px;
+}
+
+.plan-card {
+  background-color: #fab091;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  min-height: 240px;
+  max-width: 255px;
+}
+
+.plan-card h3 {
+  margin: 0 0 10px;
+  text-align: center;
+}
+
+.price {
+  margin-top: auto;
+  text-align: center;
+}
+
+.button-group {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.continue-button {
+  flex: 1;
+  margin-right: 10px;
+  padding: 10px;
+  background-color: #ff6f3f;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  max-width: 255px;
+}
+
+.continue-button:last-child {
+  margin-right: 0;
+}
+
+.continue-button:hover {
+  background-color: #ff5a21;
+}
+
+.premium-button {
+  margin-left: 10px;
+}
 </style>
