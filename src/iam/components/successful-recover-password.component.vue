89b41ccc596b54c981f13c547b92d040b1aa3699
Ricardo <richiabv@gmail.com>
2024-10-11 17:41:41 -0500

    feat(successful-recover-password): added styles for successful recover password component

--- src/iam/components/successful-recover-password.component.vue
+++ src/iam/components/successful-recover-password.component.vue
@@ -31,5 +31,102 @@ export default {
   </div>
 </template>
 <style scoped>
+.login-page {
+  display: flex;
+  height: 100vh;
+}
+
+.background-section {
+  width: 720px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+
+.restaurant-image {
+  width: 110%;
+  height: 100vh;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: -351px;
+}
+
+.background-text {
+  position: relative;
+  z-index: 1;
+  padding: 20px;
+  max-width: 300px;
+  left: -350px;
+  text-align: left;
+  word-break: keep-all;
+  max-width: 450px;
+}
+
+.background-text h2 {
+  font-size: 36px;
+  line-height: 1.2;
+  margin: 0;
+  white-space: normal;
+}
+.login-section {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: #fdeae8;
+  padding: 20px;
+}
+
+.Logo {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 150px;
+}
+
+.login-section h1 {
+  font-size: 40px;
+  margin: 20px 0;
+}
+
+.form-group {
+  width: 100%;
+  max-width: 400px;
+  margin-bottom: 20px;
+}
+
+label {
+  display: block;
+  margin-bottom: 15px;
+}
+
+input {
+  width: 458px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.go-to-login-button {
+  width: 458px;
+  max-width: 458px;
+  padding: 10px;
+  background-color: #ff6f3f;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  margin-top: 10px;
+}
+
+.go-to-login-button:hover {
+  background-color: #ff5a21;
+}
 
 </style>
