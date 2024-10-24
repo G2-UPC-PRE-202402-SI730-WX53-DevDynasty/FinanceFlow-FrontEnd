15ee33c012cc115f0f23376fc764e92ddd54898a
FABIOLA <ximemegan1302@gmail.com>
2024-10-24 01:33:54 -0500

    fix: fixed routes for my-orders

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,37 @@
-/*import {createRouter} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
+import MyOrderComponent from "@/order/pages/my-order.component.vue";
+import OrderDetailsComponent from "@/order/pages/order-details.component.vue";
+import OrderTrackerComponent from "@/order/pages/order-tracker.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/team', name:'team-activities'
+        // Home
+
+
+        // Restaurants
+
+        // Orders
+        {path: '/my-order', name: 'my-order', component: MyOrderComponent, meta: {title: 'My Orders'}},
+        {path: '/order-details/:orderId', name: 'order-details', component: OrderDetailsComponent, meta: {title: 'Order Details'}},
+        {path: '/order-tracker', name: 'order-tracker', component: OrderTrackerComponent, meta: {title: 'Order Tracker'}},
+
+
+        //Team Activities
+
+        // Profile
+
+
+        // Notifications
+
+
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'GastroGo';
+    document.title = `${baseTitle} - ${to.meta['title']}`;
+    next();
 })
 
-*/
+export default router;
