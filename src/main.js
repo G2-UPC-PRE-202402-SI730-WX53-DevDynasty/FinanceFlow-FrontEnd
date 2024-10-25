38bd8a76628e454b2355e731f81d7a7582ad236a
Marco <m.gongora.sanchez08@gmail.com>
2024-10-25 03:16:50 -0500

    feat(main): add components for restaurant and order details view

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import {PrimeVue} from "@primevue/core";
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from "primevue/config";
 import i18n from "@/i18n.js";
 import router from './routes/index.js';
-import InputText from "primevue/inputtext";
 
+import InputText from "primevue/inputtext";
 import Button from "primevue/button";
-import Dropdown from "primevue/dropdown";
 import Card from "primevue/card";
 import Avatar from 'primevue/avatar';
+import Dialog from 'primevue/dialog';
+import CascadeSelect from 'primevue/cascadeselect';
+
 
 const app = createApp(App);
 
-app.use(PrimeVue, {ripple: true});
+app.use(PrimeVue, { ripple: true });
 app.component('p-input-text', InputText);
-app.component('p-dropdown', Dropdown);
 app.component('p-button', Button);
 app.component('p-card', Card);
 app.component('p-avatar', Avatar);
+app.component('p-dialog', Dialog);
+app.component('p-cascade-select', CascadeSelect);
 
 app.use(i18n);
-
 app.use(router);
 
 app.mount('#app');
