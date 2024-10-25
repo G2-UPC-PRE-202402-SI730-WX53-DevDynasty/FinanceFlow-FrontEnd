afdc5277d32c07b99896084cc94dc8b5bd06c8a2
Marco <m.gongora.sanchez08@gmail.com>
2024-10-25 03:15:06 -0500

    feat(index): add route for order details view

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import RestaurantView from '@/restaurant/components/restaurant-view.vue';
 import RestaurantDetailsView from '@/restaurant/components/restaurant-details-view.vue';
 import RestaurantMenuView from "@/restaurant/components/restaurant-menu-view.vue";
+import RestaurantOrderDetailsView from "@/restaurant/components/restaurant-order-details-view.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -23,6 +24,11 @@ const router = createRouter({
             path: '/restaurant/:name/menu',
             name: 'restaurant-menu-view',
             component: RestaurantMenuView
+        },
+        {
+            path: '/restaurants/:name/order-details',
+            name: 'restaurant-order-details',
+            component: RestaurantOrderDetailsView
         },
         // Orders
 
